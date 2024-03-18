ec735d7da018cdb2371be70ea22dc8bd029230a0
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-03-18 21:33:58 +0200

    Add more tests

--- tests.py
+++ tests.py
@@ -22,20 +22,34 @@ class CircuitSynthesisTests(unittest.TestCase):
         print(circuit)
         self.check_correctness(circuit, truth_tables)
 
-    def test_simple_synthesis(self):
-        self.check_synthesis(Circuit(["x0", "x1"]), ["0011"])
+    def check_shannen_synthesis(self, circuit: Circuit, truth_tables: List[str], split_count: int):
+        CircuitSynthesis.shannen_algorithm(circuit, list(range(len(circuit.input_labels))), split_count, truth_tables)
+        print(circuit)
+        self.check_correctness(circuit, truth_tables)
 
-    def test_synthesis(self):
-        self.check_synthesis(Circuit(["x0", "x1", "x2"]), ["00110100"])
+    def test_simple_simple_shannen_synthesis(self):
+        self.check_shannen_synthesis(Circuit(["x0", "x1"]), ["1111"], 1)
 
-    def test_synthesis_all_zeros(self):
-        self.check_synthesis(Circuit(["x0", "x1", "x2"]), ["00000000"])
 
-    def test_synthesis_one_one(self):
-        self.check_synthesis(Circuit(["x0", "x1", "x2", "x3"]), ["0000010000000000"])
+    # def test_simple_s2(self):
+    #     circuit = Circuit(["x0", "x1", "x2"])
+    #     CircuitSynthesis.gen_all_functions(circuit, [0, 1, 2], "try")
+    #     print(circuit)
 
-    def test_synthesis_multi(self):
-        self.check_synthesis(Circuit(["x0", "x1", "x2"]), ["00110100", "11101111", "01010101"])
+    # def test_simple_synthesis(self):
+    #     self.check_synthesis(Circuit(["x0", "x1"]), ["0011"])
+    #
+    # def test_synthesis(self):
+    #     self.check_synthesis(Circuit(["x0", "x1", "x2"]), ["00110100"])
+    #
+    # def test_synthesis_all_zeros(self):
+    #     self.check_synthesis(Circuit(["x0", "x1", "x2"]), ["00000000"])
+    #
+    # def test_synthesis_one_one(self):
+    #     self.check_synthesis(Circuit(["x0", "x1", "x2", "x3"]), ["0000010000000000"])
+    #
+    # def test_synthesis_multi(self):
+    #     self.check_synthesis(Circuit(["x0", "x1", "x2"]), ["00110100", "11101111", "01010101"])
 
 
 if __name__ == '__main__':
